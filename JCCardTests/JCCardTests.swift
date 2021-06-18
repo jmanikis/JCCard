7ac8b5c1d393e842c294b2a8470a1e6aa3ac123d
JC Manikis <jmanikis@icloud.com>
2021-06-18 12:57:40 -0400

    Initial Commit

---
+++ JCCardTests/JCCardTests.swift
@@ -0,0 +1,33 @@
+//
+//  JCCardTests.swift
+//  JCCardTests
+//
+//  Created by JC Manikis on 2021-06-18.
+//
+
+import XCTest
+@testable import JCCard
+
+class JCCardTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
